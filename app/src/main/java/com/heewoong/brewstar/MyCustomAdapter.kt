610fe7ed9687c3ec388d6433d96ccfe6302215ad
oliheilnany <hanis@kaist.ac.kr>
2024-01-08 06:18:21 +0900

    tab1 making 2. EXCEPT CustomGuide

--- app/src/main/java/com/heewoong/brewstar/MyCustomAdapter.kt
+++ app/src/main/java/com/heewoong/brewstar/MyCustomAdapter.kt
@@ -1,12 +1,16 @@
 package com.heewoong.brewstar
 
-import android.content.Intent
+import android.app.AlertDialog
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.heewoong.brewstar.databinding.ActivityMyCustomEditBinding
 
 class MyCustomAdapter(private var myCustomItemList: ArrayList<MyCustomsItem>) :
         RecyclerView.Adapter<MyCustomAdapter.MyCustomViewHolder>() {
@@ -20,12 +24,56 @@ class MyCustomAdapter(private var myCustomItemList: ArrayList<MyCustomsItem>) :
         holder.bind(myCustomItemList[position])
 
         // 수정 버튼을 누르면 수정 창으로 넘어가기
-        // 이거 안 된다. 다시 해봐야지...
+        // 여기서 데이터들 다 받아와서 list에 반영까지 다 하기
         holder.btn_edit.setOnClickListener {
-            fun onClick(view: View) {
-                val intent = Intent(view.context, MyCustomEdit::class.java)
-                view.context.startActivity(intent)
+            val builder: AlertDialog.Builder = AlertDialog.Builder(holder.itemView.context, R.style.AlertDialogTheme)
+            var bindingPopup = ActivityMyCustomEditBinding.inflate(LayoutInflater.from(holder.itemView.context))
+            val view: View = bindingPopup.layoutPopup
+
+            val oldName: String = holder.tv_name.text.toString()
+            val oldMenu: String = holder.tv_menu.text.toString()
+            val oldCustom: String = holder.tv_custom.text.toString()
+            val oldLikes: String = holder.tv_likes.text.toString()
+
+            builder.setView(view)
+            view.findViewById<EditText>(R.id.editPopupName).setText(oldName)
+            view.findViewById<EditText>(R.id.editPopupMenu).setText(oldMenu)
+            view.findViewById<EditText>(R.id.editPopupCustom).setText(oldCustom)
+            view.findViewById<TextView>(R.id.popuplikes).setText(oldLikes)
+//            val intent = Intent(holder.itemView.context, MyCustomEdit::class.java)
+//            intent.putExtra("old name", oldName)
+//            holder.itemView.context.startActivity(intent)
+            val alertDialog: AlertDialog = builder.create()
+
+            // close 버튼 누르면 다시 돌아가기
+            view.findViewById<ImageButton>(R.id.popupCloseBtn).setOnClickListener {
+                alertDialog.dismiss()
             }
+
+            var newName: String = ""
+            var newMenu: String = ""
+            var newCustom: String = ""
+
+            // save 버튼 누르면 추가되기
+            view.findViewById<Button>(R.id.popupSaveBtn).setOnClickListener {
+                newName += view.findViewById<EditText>(R.id.editPopupName).text.toString()
+                newMenu += view.findViewById<EditText>(R.id.editPopupMenu).text.toString()
+                newCustom += view.findViewById<EditText>(R.id.editPopupCustom).text.toString()
+
+                alertDialog.dismiss()
+                
+                // 좋아요 수는 그대로 유지해야 함
+                myCustomItemList.add(MyCustomsItem(newName, newMenu, newCustom, oldLikes))
+                removeMyCustomItem(position)
+            }
+
+
+            // 다이얼로그 형태 지우기
+            if(alertDialog.window != null) {
+                alertDialog.window!!.setBackgroundDrawable(ColorDrawable(0))
+            }
+
+            alertDialog.show()
         }
 
         // 전체 네모 버튼을 눌렀을 때도 상세정보 보기 해야하나?
--- app/src/main/java/com/heewoong/brewstar/tab1.kt
+++ app/src/main/java/com/heewoong/brewstar/tab1.kt
@@ -1,19 +1,22 @@
 package com.heewoong.brewstar
 
-import android.content.Intent
+import android.app.AlertDialog
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.ToggleButton
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.heewoong.brewstar.databinding.ActivityMyCustomAddBinding
 import com.heewoong.brewstar.databinding.FragmentTab1Binding
 
 class tab1 : Fragment() {
@@ -21,25 +24,32 @@ class tab1 : Fragment() {
     private lateinit var binding: FragmentTab1Binding
     private lateinit var myCustomAdapter: MyCustomAdapter
     private lateinit var favoriteAdapter: FavoriteAdapter
+    private lateinit var collectAdapter: FavoriteAdapter
     private lateinit var rv_favorite: RecyclerView
+    private lateinit var rv_collect: RecyclerView
     private lateinit var rv_mycustom: RecyclerView
     //private lateinit var btn_edit: Button
     private lateinit var btn_add: ImageButton
     private lateinit var toggle: ToggleButton
+    private lateinit var btn_back: ImageButton
 
     private var myCustomItemList = ArrayList<MyCustomsItem>()
     private var favoriteItemList = ArrayList<FavoriteItem>()
+    private var collectItemList = ArrayList<FavoriteItem>()
 
     // 토글 켤 때 다 invisible하기 위해서
     private lateinit var rectangle1: ImageView
     private lateinit var rectangle2: ImageView
     private lateinit var rectangle3: ImageView
-    private lateinit var menu1: ImageView
-    private lateinit var menu2: ImageView
-    private lateinit var menu3: ImageView
-    private lateinit var text1: TextView
-    private lateinit var text2: TextView
-    private lateinit var text3: TextView
+    private lateinit var menu1: ImageButton
+    private lateinit var menu2: ImageButton
+    private lateinit var menu3: ImageButton
+    private lateinit var text1: Button
+    private lateinit var text2: Button
+    private lateinit var text3: Button
+
+    //popup창을 위한 준비들
+    private lateinit var bindingPopup: ActivityMyCustomAddBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,11 +64,6 @@ class tab1 : Fragment() {
         turnToggle()
         getAllMyCustom()
 
-        return binding.root
-    }
-
-
-    private fun turnToggle() {
         rectangle1 = binding.favoriteRectangleCoffee
         rectangle2 = binding.favoriteRectangleNoncoffee
         rectangle3 = binding.favoriteRectangleFrappuccino
@@ -68,7 +73,105 @@ class tab1 : Fragment() {
         text1 = binding.tvFavoriteMenuCoffee
         text2 = binding.tvFavoriteMenuNoncoffee
         text3 = binding.tvFavoriteMenuFrappuccino
+        btn_back = binding.btnBack
 
+        collectAdapter = FavoriteAdapter(collectItemList)
+        rv_collect = binding.rvCollectOne
+        // 가로로 스크롤 하도록
+        rv_collect.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+        rv_collect.adapter = collectAdapter
+
+        for (i: Int in 1..10) {
+            collectItemList.add(
+                FavoriteItem(
+                    "레몬 아샷추 하핫",
+                    "샷2 + 레몬시럽1 + 복숭아티백 + 헤헤 + 호호"
+                )
+            )
+        }
+        
+        // favorite Coffee 눌렀을 때
+        menu1.setOnClickListener {
+            menu1.setImageResource(R.drawable.coffee)
+            text1.setText("Coffee")
+            clickCollectOne()
+        }
+        text1.setOnClickListener {
+            menu1.setImageResource(R.drawable.coffee)
+            text1.setText("Coffee")
+            clickCollectOne()
+        }
+        
+        // favorite Non Coffee 눌렀을 때
+        menu2.setOnClickListener {
+            menu1.setImageResource(R.drawable.noncoffee)
+            text1.setText("Non Coffee")
+            clickCollectOne()
+        }
+        text2.setOnClickListener {
+            menu1.setImageResource(R.drawable.noncoffee)
+            text1.setText("Non Coffee")
+            clickCollectOne()
+        }
+        
+        // favorite Frappuccino 눌렀을 때
+        menu3.setOnClickListener {
+            menu1.setImageResource(R.drawable.frappuccino)
+            text1.setText("Frappuccino")
+            clickCollectOne()
+        }
+        text3.setOnClickListener {
+            menu1.setImageResource(R.drawable.frappuccino)
+            text1.setText("Frappuccino")
+            clickCollectOne()
+        }
+
+        // 다시 back버튼 누르면
+        btn_back.setOnClickListener {
+            clickBack()
+        }
+
+        
+        return binding.root
+    }
+
+
+    private fun clickCollectOne() {
+        menu1.isEnabled = false
+        text1.isEnabled = false
+        rectangle2.visibility = INVISIBLE
+        rectangle3.visibility = INVISIBLE
+        menu2.visibility = INVISIBLE
+        menu3.visibility = INVISIBLE
+        text2.visibility = INVISIBLE
+        text3.visibility = INVISIBLE
+
+        rv_collect.visibility = VISIBLE
+        btn_back.visibility = VISIBLE
+    }
+
+    private fun clickBack() {
+        menu1.isEnabled = true
+        text1.isEnabled = true
+        btn_back.visibility = INVISIBLE
+        menu1.setImageResource(R.drawable.coffee)
+        text1.setText("Coffee")
+
+        rectangle1.visibility = VISIBLE
+        rectangle2.visibility = VISIBLE
+        rectangle3.visibility = VISIBLE
+        menu1.visibility = VISIBLE
+        menu2.visibility = VISIBLE
+        menu3.visibility = VISIBLE
+        text1.visibility = VISIBLE
+        text2.visibility = VISIBLE
+        text3.visibility = VISIBLE
+
+        rv_collect.visibility = INVISIBLE
+    }
+
+
+    private fun turnToggle() {
         rv_favorite = binding.rvFavorite
 
 
@@ -93,6 +196,8 @@ class tab1 : Fragment() {
         toggle.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 // 토글이 켜질 때
+                menu1.setImageResource(R.drawable.coffee)
+                text1.setText("Coffee")
                 rectangle1.visibility = INVISIBLE
                 rectangle2.visibility = INVISIBLE
                 rectangle3.visibility = INVISIBLE
@@ -104,8 +209,12 @@ class tab1 : Fragment() {
                 text3.visibility = INVISIBLE
 
                 rv_favorite.visibility = VISIBLE
+                rv_collect.visibility = INVISIBLE
+                btn_back.visibility = INVISIBLE
             } else {
                 // 토글이 꺼질 때
+                menu1.setImageResource(R.drawable.coffee)
+                text1.setText("Coffee")
                 rectangle1.visibility = VISIBLE
                 rectangle2.visibility = VISIBLE
                 rectangle3.visibility = VISIBLE
@@ -117,6 +226,8 @@ class tab1 : Fragment() {
                 text3.visibility = VISIBLE
 
                 rv_favorite.visibility = INVISIBLE
+                rv_collect.visibility = INVISIBLE
+                btn_back.visibility = INVISIBLE
             }
         }
     }
@@ -140,10 +251,50 @@ class tab1 : Fragment() {
 
         btn_add = binding.btnAdd
         btn_add.setOnClickListener {
-            val intent = Intent(activity, MyCustomAdd::class.java)
-            startActivityForResult(intent, PHONE_ADD_REQUEST_CODE)
+            showCustomDialog()
+//            val intent = Intent(activity, MyCustomAdd::class.java)
+//            startActivityForResult(intent, PHONE_ADD_REQUEST_CODE)
         }
     }
+
+    private fun showCustomDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext(), R.style.AlertDialogTheme)
+        bindingPopup = ActivityMyCustomAddBinding.inflate(layoutInflater)
+        val view: View = bindingPopup.layoutPopup
+
+        builder.setView(view)
+
+        val alertDialog: AlertDialog = builder.create()
+
+        // close 버튼 누르면 다시 돌아가기
+        view.findViewById<ImageButton>(R.id.popupCloseBtn).setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        var newName: String = ""
+        var newMenu: String = ""
+        var newCustom: String = ""
+        var newLikes: String = "0"
+
+        // save 버튼 누르면 추가되기
+        view.findViewById<Button>(R.id.popupSaveBtn).setOnClickListener {
+            newName += view.findViewById<EditText>(R.id.editPopupName).text.toString()
+            newMenu += view.findViewById<EditText>(R.id.editPopupMenu).text.toString()
+            newCustom += view.findViewById<EditText>(R.id.editPopupCustom).text.toString()
+
+            alertDialog.dismiss()
+
+            myCustomItemList.add(MyCustomsItem(newName, newMenu, newCustom, newLikes))
+            myCustomAdapter.notifyDataSetChanged()
+        }
+
+        // 다이얼로그 형태 지우기
+        if(alertDialog.window != null) {
+            alertDialog.window!!.setBackgroundDrawable(ColorDrawable(0))
+        }
+        alertDialog.show()
+    }
+
 
     companion object {
         private const val PHONE_ADD_REQUEST_CODE = 1
